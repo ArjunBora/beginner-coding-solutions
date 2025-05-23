da1fc4b9704eaf71e347071e8fb3a92bf51f26a8
Arjun Bora <arjunbora099@gmail.com>
2025-05-23 23:26:49 +0530

    Create Div-with-Rem.py

---
+++ Basic/Div-with-Rem.py
@@ -0,0 +1,6 @@
+# Q.Write a program where the user is asked to enter two integers (divisor and dividend) and the quotient and the remainder of their division is computed.(Both divisor and dividend should be integers.)
+
+dividend = int(input("Enter dividend: "))
+divisor = int(input("Enter divisor: "))
+print("Quotient:", dividend // divisor)
+print("Remainder:", dividend % divisor)
