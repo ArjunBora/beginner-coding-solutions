52dcde33c24edc1b91028ed1823efe21a747ebb1
Arjun Bora <arjunbora099@gmail.com>
2025-05-24 11:13:21 +0530

    Update Div-with-Rem.py

--- Basic/Div-with-Rem.py
+++ Basic/Div-with-Rem.py
@@ -1,4 +1,5 @@
-# Q.Write a program where the user is asked to enter two integers (divisor and dividend) and the quotient and the remainder of their division is computed.(Both divisor and dividend should be integers.)
+# Q.Write a program where the user is asked to enter two integers (divisor and dividend) 
+# and the quotient and the remainder of their division is computed.(Both divisor and dividend should be integers.)
 
 dividend = int(input("Enter dividend: "))
 divisor = int(input("Enter divisor: "))
