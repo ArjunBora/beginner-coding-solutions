53f26b54bd4d146b6e9e3df49924f66c306e19fc
Arjun Bora <arjunbora099@gmail.com>
2025-05-24 10:08:03 +0530

    Create Check_prime.py

---
+++ Loops/Check_prime.py
@@ -0,0 +1,12 @@
+#Q.  Write a Program to Check Whether a Number is Prime or Not
+
+num = int(input("Enter a number: "))
+if num <= 1:
+    print("Not prime")
+else:
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            print("Not prime")
+            break
+    else:
+        print("Prime")
