a1f90462f5a41af9f53511c64e88a719f54b65f3
Arjun Bora <arjunbora099@gmail.com>
2025-05-24 11:12:07 +0530

    Update Quadratic_Equation.py

--- If-else/Quadratic_Equation.py
+++ If-else/Quadratic_Equation.py
@@ -1,4 +1,5 @@
-#Q. Write a Program which accepts coefficients of a quadratic equation from the user and displays the roots (both real and complex roots depending upon the discriminant)
+#Q. Write a Program which accepts coefficients of a quadratic equation from the user and displays the roots 
+#(both real and complex roots depending upon the discriminant)
 
 import math
 
