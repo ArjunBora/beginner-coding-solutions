cb93cee5d71c6b066db2ea3fc496891667902e89
Arjun Bora <arjunbora099@gmail.com>
2025-05-24 10:04:28 +0530

    Create Using_pow().py

---
+++ Loops/Using_pow().py
@@ -0,0 +1,5 @@
+#Q. Write a Program to Calculate Power of a Number using inbuilt pow() function by taking two inputs from users as Base and exponent respectively
+
+base = float(input("Enter base: "))
+exponent = float(input("Enter exponent: "))
+print("Result:", pow(base, exponent))
