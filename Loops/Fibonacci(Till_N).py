1d227bb39c332228baf893eada72839be1f3b32d
Arjun Bora <arjunbora099@gmail.com>
2025-05-24 11:11:15 +0530

    Update Fibonacci(Till_N).py

--- Loops/Fibonacci(Till_N).py
+++ Loops/Fibonacci(Till_N).py
@@ -1,4 +1,6 @@
-"""Q. Write a Program to Display Fibonacci Series upto certain number n (n is entered by user)
+"""
+Q. Write a Program to Display Fibonacci Series upto certain number n (n is entered by user)
+
 Example: n=100
 Output:
 Fibonacci Series: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89
