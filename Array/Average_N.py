1ff1e177b78efe76eeaba350c12fcb29aa71c8b4
Arjun Bora <arjunbora099@gmail.com>
2025-05-24 11:12:48 +0530

    Update Average_N.py

--- Array/Average_N.py
+++ Array/Average_N.py
@@ -1,4 +1,5 @@
-#Q. Write a Program that takes N elements (max. value of N is 100 and N is the float number specified by user) from user, stores data in an array and Calculates the average of those numbers.
+#Q. Write a Program that takes N elements (max. value of N is 100 and N is the float number specified by user) 
+#from user, stores data in an array and Calculates the average of those numbers.
 
 n = int(input("Enter N (max 100): "))
 arr = []
