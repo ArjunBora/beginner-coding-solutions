9e05dae5230b3baaffd86868b0997bdfd8b512f0
Arjun Bora <arjunbora099@gmail.com>
2025-05-24 06:36:59 +0530

    Create Multiply_Decimals.py

---
+++ Basic/Multiply_Decimals.py
@@ -0,0 +1,5 @@
+#Q. Write a Program to Multiply two decimal Numbers entered by User
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+print("Product:", num1 * num2)
