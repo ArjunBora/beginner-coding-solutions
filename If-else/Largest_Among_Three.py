9c1cad4ea9fb5c27ac388a6bf8a586b231b28f04
Arjun Bora <arjunbora099@gmail.com>
2025-05-24 08:41:02 +0530

    Create Largest_Among_Three.py

---
+++ If-else/Largest_Among_Three.py
@@ -0,0 +1,6 @@
+#Q. Write a Program to Find Largest Number Among Three Numbers entered by users
+
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+c = float(input("Enter third number: "))
+print("Largest:", max(a, b, c))
