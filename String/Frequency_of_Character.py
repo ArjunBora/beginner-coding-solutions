dc9e8215119b156aa33eab0124284ee2006b8469
Arjun Bora <arjunbora099@gmail.com>
2025-05-24 10:55:14 +0530

    Create Frequency_of_Character.py

---
+++ String/Frequency_of_Character.py
@@ -0,0 +1,5 @@
+#Q. Write a Program to Find the Frequency of given Character by user in a String
+
+string = input("Enter a string: ")
+char = input("Enter a character: ")
+print("Frequency:", string.count(char))
