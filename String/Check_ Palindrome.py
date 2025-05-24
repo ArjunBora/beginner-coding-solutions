23cc466ab51c3d915ee46e3a21f08fd64304b6ec
Arjun Bora <arjunbora099@gmail.com>
2025-05-24 11:03:51 +0530

    Create Check_ Palindrome.py

---
+++ String/Check_ Palindrome.py
@@ -0,0 +1,5 @@
+#Q. Write a C++ program to check if a given string is a Palindrome or not.
+
+string = input("Enter a string: ")
+clean_str = ''.join(ch.lower() for ch in string if ch.isalnum())
+print("Palindrome" if clean_str == clean_str[::-1] else "Not Palindrome")
