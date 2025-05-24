b73b44c9ecd6c25ae74bad39f6747d493d1310f5
Arjun Bora <arjunbora099@gmail.com>
2025-05-24 06:16:42 +0530

    Create Swap_Numbers.py

---
+++ Basic/Swap_Numbers.py
@@ -0,0 +1,6 @@
+#Q. Write a Program to Swap Two Numbers
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+a, b = b, a
+print("After swapping - First:", a, "Second:", b)
