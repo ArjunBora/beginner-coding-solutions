4aa59b985e436144acc273b3a7390fbc898a0837
Arjun Bora <arjunbora099@gmail.com>
2025-05-24 11:00:35 +0530

    Create Concatenate_String.py

---
+++ String/Concatenate_String.py
@@ -0,0 +1,5 @@
+#Q. Write a Program to Concatenate (join) Two Strings entered by user
+
+str1 = input("Enter first string: ")
+str2 = input("Enter second string: ")
+print("Concatenated:", str1 + str2)
