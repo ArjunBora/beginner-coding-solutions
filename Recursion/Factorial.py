e112ec5b3b8825884b36fb6484627fdfe5b9340d
Arjun Bora <arjunbora099@gmail.com>
2025-05-25 13:34:15 +0530

    Delete Recursion directory

--- Recursion/Factorial.py
+++
@@ -1,7 +0,0 @@
-#Q. Write a Program to Calculate Factorial of a Number Using Recursion
-
-def factorial(n):
-    return 1 if n == 0 else n * factorial(n-1)
-
-n = int(input("Enter N: "))
-print("Factorial:", factorial(n))
--- Recursion/Sum_N.py
+++
@@ -1,7 +0,0 @@
-#Q. Write a Program to Find Sum of N Natural Numbers (entered by users) using Recursion
-
-def sum_n(n):
-    return 1 if n == 1 else n + sum_n(n-1)
-
-n = int(input("Enter N: "))
-print("Sum:", sum_n(n))
